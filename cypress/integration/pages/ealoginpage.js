dd20e7afc5eed852f9e1909ca2f98d7305de1ed0
Lesley Boyd <lesley@blackthorn.io>
2021-01-14 17:24:42 -0600

    added xpath plugin and refactored with xpath ref

--- cypress/integration/pages/ealoginpage.js
+++ cypress/integration/pages/ealoginpage.js
@@ -3,8 +3,8 @@
 export class EALoginPage {
 
     performLogin(userName, password){
-        cy.get('#UserName').type(userName);
-        cy.get('#Password').type(password, {log:false});
+        cy.xpath('//input[@id="UserName"]').type(userName);
+        cy.xpath('//input[@id="Password"]').type(password, {log:false});
     }
 
     clickLoginButton(){
